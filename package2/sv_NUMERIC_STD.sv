1879a59e23921ecc77d881c3fce40c9c7649d80b
Agfra167 <104153729+Agfra167@users.noreply.github.com>
2022-04-22 10:20:20 +0530

    Add files via upload

---
+++ package2/sv_NUMERIC_STD.sv
@@ -0,0 +1,12 @@
+// FP-UVM - UVM for FPGAs app 
+// Automatically generated from VHDL Package: NUMERIC_STD 
+package sv_NUMERIC_STD; 
+  parameter CopyRightNotice = -1063088084;
+  parameter NAU = -1063088084;
+  parameter NAS = -1063088084;
+  parameter NO_WARNING = 0;
+  parameter MATCH_TABLE = 0;
+endpackage : sv_NUMERIC_STD 
+
+import sv_NUMERIC_STD::* 
+
